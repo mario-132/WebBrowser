4428c9a389c706df7e0486b5519a48449c10c3db
mario-132 <timleijten132@gmail.com>
2020-11-18 20:36:19 +0100

    addition to previous, remove most stuff, use libcss

--- HTMLRenderer/main.cpp
+++ HTMLRenderer/main.cpp
@@ -3,10 +3,8 @@
 #include <gumbo.h>
 #include "freetypeeasy.h"
 #include "x11window.h"
-#include "htmlrenderer.h"
 #include "webservice.h"
 #include "renderdom.h"
-#include "cssparse.h"
 #include "debugger.h"
 #include <chrono>
 
@@ -173,40 +171,12 @@ std::string resolvePath(std::string path, std::string fullURL)
     return newPath;
 }
 
-void renderDocumentBox(RDocumentBox &documentBox, int yScroll)
-{
-    unsigned char r = rand()%255;
-    unsigned char g = rand()%255;
-    unsigned char b = rand()%255;
-    for (int x = documentBox.X; x < documentBox.X + documentBox.W; x++)
-    {
-        for (int y = documentBox.Y + yScroll; y < documentBox.Y + documentBox.H + yScroll; y++)
-        {
-            if (x == documentBox.X || y == documentBox.Y + yScroll ||
-                    x == (documentBox.X + documentBox.W) - 1 ||
-                    y == (documentBox.Y + documentBox.H + yScroll) - 1)
-            {
-                if (x < 0 || y < 0 || x >= FRAMEBUFFER_WIDTH || y >= FRAMEBUFFER_HEIGHT)
-                    continue;
-                framebuffer[(y*FRAMEBUFFER_WIDTH*3)+(x*3)] = r;
-                framebuffer[(y*FRAMEBUFFER_WIDTH*3)+(x*3)+1] = g;
-                framebuffer[(y*FRAMEBUFFER_WIDTH*3)+(x*3)+2] = b;
-            }
-        }
-    }
-    for (int i = 0; i < documentBox.childBoxes.size(); i++)
-    {
-        renderDocumentBox(documentBox.childBoxes[i], yScroll);
-    }
-}
-
 class WebPage
 {
 public:
     std::string pageTitle;// Temporary way to get page title.
     RenderDOM dom;
-    RenderDOMItem rootDomItem;
-    HTMLRenderer htmlrenderer;
+
     GumboOutput* output;
     ~WebPage()
     {
@@ -215,45 +185,13 @@ public:
 
     void init(std::string html, std::string fullURL)
     {
-
-        //std::string htmlFile = htmlFileLoader("/home/tim/Documents/Development/gumbo-parser/docs/html/index.html");
-        //std::string htmlFile = htmlFileLoader("/home/tim/Documents/Development/WebBrowserData/HTML/simple_text.html");
-        //std::string htmlFile = htmlFileLoader("/home/tim/Documents/Development/WebBrowserData/HTML/Wikipedia.html");
-        //std::string htmlFile = htmlFileLoader("/home/tim/Documents/Development/WebBrowserData/HTML/Wikipedia - Wikipedia.html");
-        //std::string htmlFile = htmlFileLoader("/home/tim/Documents/Development/WebBrowserData/HTML/HTML/Google.html");
-        //std::string htmlFile = htmlFileLoader("/home/tim/Documents/Development/WebBrowserData/HTML/es64f4.html");
-        //std::string htmlFile = htmlFileLoader("/home/tim/Documents/Development/WebBrowserData/HTML/Home - MangaDex.html");
-        //std::string htmlFile = htmlFileLoader("/home/tim/Documents/Development/WebBrowserData/HTML/reddit_ the front page of the internet.html");
-        //std::string htmlFile = htmlFileLoader("/home/tim/Documents/Development/WebBrowserData/HTML/mario-132 · GitHub.html");
-
-        //std::string htmlFile = WebService::htmlFileDownloader("https://www.w3schools.com/w3css/tryw3css_templates_band.htm");
-        //std::string htmlFile = WebService::htmlFileDownloader("https://lightboxengine.com/codeview.html");
-        //std::string htmlFile = WebService::htmlFileDownloader("https://lightboxengine.com/multiselector.html");
         std::string htmlFile = html;
-        //std::string htmlFile = WebService::htmlFileDownloader("https://htmlyoutube.lightboxengine.com");
-        //std::string htmlFile = WebService::htmlFileDownloader("https://myanimelist.net/animelist/timl132?status=7");
 
         findAndReplaceAll( htmlFile, "&nbsp;", " ");
 
         output = gumbo_parse(htmlFile.c_str());
 
-
         getTitleFromDOM(output->root, pageTitle, false);
-
-        RenderDOMStyle style;
-        style.display = "inline";
-        style.font_size = Debugger::getAdjustmentGetValue("textsize_drag");
-        style.line_height = 1.2;
-        style.bold = false;
-        style.isLink = false;
-        style.color.r = 0;
-        style.color.g = 0;
-        style.color.b = 0;
-        style.color.a = 255;
-        style.background_color.r = 255;
-        style.background_color.g = 255;
-        style.background_color.b = 255;
-        style.background_color.a = 0;
 
         std::string css = htmlFileLoader("/home/tim/Documents/Development/WebBrowserData/HTML/default.css");
         std::vector<std::string> stylesheetPaths;
@@ -265,141 +203,16 @@ public:
             std::cout << "Downloaded stylesheet: " << newsrc << std::endl;
         }
 
-        css = css::commentRemover(css);
         findAndReplaceAll(css, "!important", "");
-        std::vector<css::CSSSelectorBlock> cssOut = css::parseFromString(css);
-
-        dom.domCallStack.clear();
-        rootDomItem = dom.parseGumboTree(output->root, style, fullURL, cssOut);
 
         if (Debugger::getCheckboxEnabled("debug_log_css"))
         {
-            std::string cssString;
-            for (unsigned int i = 0; i < cssOut.size(); i++)
-            {
-                cssString += "<";
-                for (int j = 0; j < cssOut[i].selectors.size(); j++)
-                {
-                    cssString += cssOut[i].selectors[j].additionals.back().name;
-                    if (cssOut[i].selectors[j].additionals.back().matchingClasses.size())
-                    {
-                        cssString += "(";
-                        for (int k = 0; k < cssOut[i].selectors[j].additionals.back().matchingClasses.size(); k++)
-                        {
-                            cssString += cssOut[i].selectors[j].additionals.back().matchingClasses[k];
-                        }
-                        cssString += ")";
-                    }
-                    cssString += ", ";
-                }
-                cssString += ">\n";
-                for (int j = 0; j < cssOut[i].items.size(); j++)
-                {
-                    cssString += "  " + cssOut[i].items[j].attribute.attributeAsString;
-                    if (cssOut[i].items[j].value.type == css::CSS_TYPE_PX)
-                        cssString += " " + std::to_string(cssOut[i].items[j].value.numberValue) + "px" + "\n";
-                    else if (cssOut[i].items[j].value.type == css::CSS_TYPE_EM)
-                        cssString += " " + std::to_string(cssOut[i].items[j].value.numberValue) + "em" + "\n";
-                    else if (cssOut[i].items[j].value.type == css::CSS_TYPE_PERCENT)
-                        cssString += " " + std::to_string(cssOut[i].items[j].value.numberValue) + "%" + "\n";
-
-                    else if (cssOut[i].items[j].attribute.attributeAsString == "display")
-                    {
-                        if (cssOut[i].items[j].value.valueAsString == "block")
-                        {
-                            cssString += " block\n";
-                        }
-                        else if (cssOut[i].items[j].value.valueAsString == "inline")
-                        {
-                            cssString += " inline\n";
-                        }
-                        else if (cssOut[i].items[j].value.valueAsString == "inline-block")
-                        {
-                            cssString += " inline-block\n";
-                        }
-                        else if (cssOut[i].items[j].value.valueAsString == "none")
-                        {
-                            cssString += " none\n";
-                        }
-                        else
-                        {
-                            cssString += " UNKNOWN\n";
-                        }
-                    }
-                    else if (cssOut[i].items[j].value.type == css::CSS_TYPE_UNKNOWN)
-                        cssString += " UNKNOWN\n";
-                    else if (cssOut[i].items[j].value.type == css::CSS_TYPE_NUMBER)
-                        cssString += " " + std::to_string(cssOut[i].items[j].value.numberValue) + "\n";
-                    else if (cssOut[i].items[j].value.type == css::CSS_TYPE_COLOR)
-                        cssString += " " + std::to_string((int)cssOut[i].items[j].value.color.r) + "," + std::to_string((int)cssOut[i].items[j].value.color.g) + "," + std::to_string((int)cssOut[i].items[j].value.color.b) + "\n";
-                }
-            }
-            Debugger::setTextBoxText("css_textview", cssString);
+            Debugger::setTextBoxText("css_textview", "unavailable");
         }
-
-
-        htmlrenderer.framebuffer = framebuffer;
-        htmlrenderer.framebufferWidth = FRAMEBUFFER_WIDTH;
-        htmlrenderer.framebufferHeight = FRAMEBUFFER_HEIGHT;
     }
     void loop(X11Window &window, fte::freetypeInst *inst)
     {
-        RDocumentBox documentBox;
-        documentBox.X = Debugger::getAdjustmentGetValue("docbox_offset");
-        documentBox.Y = Debugger::getAdjustmentGetValue("docbox_offset");//window.height/3;
-        documentBox.W = window.width-(Debugger::getAdjustmentGetValue("docbox_offset")*2);
-        documentBox.maxW = documentBox.W;
-        documentBox.wLocked = true;
-        documentBox.hLocked = false;
-        documentBox.H = 0;
-        if (Debugger::getCheckboxEnabled("debug_docbox_1px"))
-            documentBox.H = 1;
-        //documentBox.textStartX = 0;
-        //documentBox.textStartY = 0;//window.height/3;
 
-        htmlrenderer.RenderItems.clear();
-        //htmlrenderer.tX = 0;
-        //htmlrenderer.tY = 100;
-        if (window.mousePressed && window.mousex > 0 && window.mousey > 0 && window.mousex < window.width && window.mousey < window.height)
-        {
-            htmlrenderer.mouseX = window.mousex;
-            htmlrenderer.mouseY = window.mousey;
-        }
-        htmlrenderer.pressed = window.mousePressed && window.mousex > 0 && window.mousey > 0 && window.mousex < window.width && window.mousey < window.height && !Debugger::getCheckboxEnabled("debug_link_disable");
-        htmlrenderer.assembleRenderListV2(rootDomItem, inst, &documentBox, RenderDOMStyle());
-        /*std::string a;
-        for (int i = 0; i < rootDomItem.element.style.cssdbg.matchingSelectorStrings.size(); i++)
-        {
-            a += rootDomItem.element.style.cssdbg.matchingSelectorStrings[i];
-        }
-        Debugger::setTextBoxText("NodeInfoTextBox", a);*/
-        /*if (window.mousePressed && window.mousex > 0 && window.mousey > 0 && window.mousex < window.width && window.mousey < window.height)
-        {
-            for (unsigned int i = 0; i < htmlrenderer.RenderItems.size(); i++)
-            {
-                if (    window.mousex > htmlrenderer.RenderItems[i].position.x &&
-                        window.mousey < htmlrenderer.RenderItems[i].position.y &&
-                        window.mousex < htmlrenderer.RenderItems[i].position.x + htmlrenderer.RenderItems[i].position.w &&
-                        window.mousey > htmlrenderer.RenderItems[i].position.y - htmlrenderer.RenderItems[i].position.h
-                        )
-                {
-                    //std::cout << activeStyle.background_color.r << std::endl;
-                    std::string selectorsDebug;
-                    for (unsigned int i = 0; i < htmlrenderer.RenderItems[i].cssdbg.matchingSelectorStrings.size(); i++)
-                    {
-                        selectorsDebug += htmlrenderer.RenderItems[i].cssdbg.matchingSelectorStrings[i] + "\n";
-                    }
-                    Debugger::setTextBoxText("NodeInfoBox2", selectorsDebug);
-                }
-            }
-        }*/
-
-        htmlrenderer.renderRenderList(inst, htmlrenderer.RenderItems);
-
-        // Draws a square outline on the document box for debug
-        srand(2);
-        if (Debugger::getCheckboxEnabled("debug_docbox"))
-            renderDocumentBox(documentBox, htmlrenderer.yScroll);
     }
 };
 
@@ -451,7 +264,6 @@ int main()
         window.setTitle("WebBrowser - " + webpage->pageTitle);
         Debugger::loop();
         //std::chrono::high_resolution_clock::time_point lastTime = std::chrono::high_resolution_clock::now();
-        webpage->htmlrenderer.yScroll = scrpos;
         webpage->loop(window, inst);
         //std::chrono::high_resolution_clock::time_point nowTime = std::chrono::high_resolution_clock::now();
         //std::cout << std::fixed << std::chrono::duration_cast<std::chrono::microseconds>( nowTime - lastTime ).count()/1000.0f << "ms" << std::endl;
@@ -466,24 +278,6 @@ int main()
                 window.displayBuffer[(y*3840*4)+(x*4)+1] = framebuffer[(ys*FRAMEBUFFER_WIDTH*3)+(x*3)+1];
                 window.displayBuffer[(y*3840*4)+(x*4)+0] = framebuffer[(ys*FRAMEBUFFER_WIDTH*3)+(x*3)+2];
             }
-        }
-
-        if (webpage->htmlrenderer.switchPage)
-        {
-            Debugger::setSpinnerEnabled("loadingSpinner", true);
-            Debugger::loop();
-            std::string href = webpage->htmlrenderer.nextpage;
-            delete webpage;
-            webpage = new WebPage();
-            Debugger::loop();
-            currentWebPage = resolvePath(href, currentWebPage);
-            std::cout << "Fetching: " << currentWebPage << std::endl;
-            std::string html = WebService::htmlFileDownloader(currentWebPage);
-            webpage->init(html, currentWebPage);
-            window.scrollPos = 0;
-            Debugger::setEntryText("UrlBox", currentWebPage);
-            Debugger::loop();
-            Debugger::setSpinnerEnabled("loadingSpinner", false);
         }
 
         if (Debugger::getWasGoButtonPressed())
--- HTMLRenderer/renderdom.h
+++ HTMLRenderer/renderdom.h
@@ -3,7 +3,6 @@
 #include <gumbo.h>
 #include <vector>
 #include <iostream>
-#include "cssparse.h"
 
 /// What to use is set in the cmake
 //#define USEMAGICK true
@@ -24,100 +23,12 @@ struct RenderDOMColor
     unsigned int a;
 };
 
-struct AdditionalDebuggingCSSInfo
-{
-    std::vector<std::string> matchingSelectorStrings;
-};
-
-struct RenderDOMStyle
-{
-    std::string display;
-    //bool visible;
-    //bool blockOrInline;// 0 = inline 1 = bloc
-    int font_size;
-    float line_height;
-    std::string text_align;
-
-    css::CSSValue width;
-    css::CSSValue height;
-
-    RenderDOMColor color;
-    RenderDOMColor background_color;
-
-    AdditionalDebuggingCSSInfo cssdbg;
-
-    bool bold;
-    bool isLink;
-};
-
-enum RenderDOMType
-{
-    RENDERDOM_ELEMENT,
-    RENDERDOM_TEXT,
-    RENDERDOM_NONE
-};
-
-struct RenderDOMAttribute
-{
-    std::string name;
-    std::string value;
-};
-
-struct RenderDOMImage
-{
-    std::string imageDataRaw;
-    unsigned char* imageData;/// Todo: @mario-132 Fix this memory leak!
-    int imgW;
-    int imgH;
-    int comp;
-
-    bool downloaded;
-    bool decoded;
-};
-
-struct RenderDOMElement
-{
-    GumboTag tag;
-    std::vector<RenderDOMAttribute> attributes;
-    RenderDOMStyle style;
-
-    RenderDOMImage image;// Only use this if tag == GUMBO_TAG_IMG
-};
-
-struct RenderDOMItem
-{
-    RenderDOMElement element;
-    std::string text;
-    RenderDOMType type;
-
-    std::vector<RenderDOMItem> children;
-};
-
-struct DOMStackItem
-{
-    std::string tag;
-    std::string unparsedClasses;
-    std::string unparsedIDs;
-};
-
 class RenderDOM/// Takes the html dom and css and puts it together
 {
 public:
     RenderDOM();
-    RenderDOMItem parseGumboTree(GumboNode *node, RenderDOMStyle lstyle, std::string fullURL, std::vector<css::CSSSelectorBlock> &css);
-    std::vector<DOMStackItem> domCallStack;
-    void applyStyle(RenderDOMItem &item, RenderDOMStyle &style);
-    bool checkSelectorMatch(std::string selector, const DOMStackItem &item);
-    std::vector<std::string> parseSpaceSeparatedString(std::string str);
-    bool checkClassMatch(std::string classname, std::string items);
-    bool checkIDMatch(std::string idname, std::string items);
-
-    void applyItemToStyle(css::CSSItem item, RenderDOMStyle &style, RenderDOMStyle oldstyle);
-
-private:
-    std::string findBasePath(std::string path);
-
-    std::string resolvePath(std::string path, std::string fullURL);
+    std::string gumboTagToString(GumboTag tag);
+    void parseGumboTree(GumboNode *node);
 };
 
 #endif // RENDERDOM_H
