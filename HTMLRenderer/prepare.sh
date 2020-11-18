eb81e7395c0db3c4d79d8a74cefb8090d508585c
mario-132 <timleijten132@gmail.com>
2020-11-18 22:57:28 +0100

    Add script to download dependencies, fix github autocompile thing

---
+++ HTMLRenderer/prepare.sh
@@ -0,0 +1,38 @@
+echo "Now downloading and installing needed libraries"
+mkdir ext
+cd ext
+
+git clone git://git.netsurf-browser.org/libwapcaplet.git
+git clone git://git.netsurf-browser.org/libparserutils.git
+git clone git://git.netsurf-browser.org/libcss.git
+git clone https://github.com/pcwalton/netsurf-buildsystem
+
+cd netsurf-buildsystem
+make
+sudo make install
+
+cd ..
+
+export LIBRARY_PATH=/opt/netsurf
+export C_INCLUDE_PATH=/opt/netsurf
+export ECHO=echo
+export GREP=grep
+export PERL=perl
+export PKGCONFIG=pkg-config
+export BUILD_CC=gcc
+
+cd libwapcaplet
+make
+sudo make install
+
+cd ..
+
+cd libparserutils
+make
+sudo make install
+
+cd ..
+
+cd libcss
+make
+sudo make install
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Web browser, under development.
-Requires some things to compile, you'll know by the errors what.
 Currently Linux only.
+
+sudo apt-get install -y libx11-dev libmagick++-6.q16-dev libcurl4-gnutls-dev libgtk2.0-dev libgtk-3-dev git gcc make libgumbo-dev
+./prepare.sh
